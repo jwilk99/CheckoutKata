10906b9df68517843f7089f1ede314b8e964f75c
Josh Wilkinson <joshwilkinson9901@gmail.com>
2025-08-30 14:11:45 +0100

    Added Interface Method Code to checkout

--- Classes/Checkout.cs
+++ Classes/Checkout.cs
@@ -14,12 +14,46 @@ namespace CheckoutKata
 
         void ICheckout.Scan(Item item)
         {
-
+            if (string.IsNullOrEmpty(item.Sku))
+                throw new ArgumentException("SKU is required");
+            if (item.UnitPrice == 0)
+                throw new ArgumentException("UnitPrice cannot be 0");
+            items.Add(item);
         }
         int ICheckout.GetTotalPrice()
         {
-            return 0;
-        }
+            if (items.Count == 0)
+                throw new ArgumentException("You have not scanned any items");
 
+            int total = 0;
+            var groupedItems = items.GroupBy(i => i.Sku).ToList();
+            foreach (var group in groupedItems)
+            {
+                var count = group.Count();
+                var item = group.First();
+
+                if (count > 1 && item.SpecialPrice != null)
+                {
+                    var specialPriceTimes = count / item.SpecialPrice.quantity;
+                    var remainder = count % item.SpecialPrice.quantity;
+                    if (specialPriceTimes > 0)
+                    {
+                        total += specialPriceTimes * item.SpecialPrice.price;
+                        total += (item.UnitPrice * remainder);
+                    }
+                    else
+                    {
+                        total += item.UnitPrice * count;
+                    }
+                }
+                else
+                {
+                    total += item.UnitPrice;
+                }
+
+            }
+
+            return total;
+        }
     }
 }
